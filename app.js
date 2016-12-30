008473296bb9f9766ec3fe7c7661f2fcbc15cf7e
Mark Sarpong <markus20@outlook.com>
2016-12-30 08:08:31 +0000

    Modified the new, index and show page

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get("/campgrounds", function(req, res){
     if(err){
       console.log(err);
     }else{
-        res.render("index", {campdata: allCampgrounds})
+        res.render("campgrounds/index", {campdata: allCampgrounds})
     }
   });
 
@@ -72,7 +72,7 @@ Campground.create(newCampground, function(err, newlyCreated){
 });
 
 app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 
@@ -88,9 +88,19 @@ app.get("/campgrounds/:id", function(req, res){
       console.log(err);
     }else{
       console.log(foundCampground);
-      res.render("show", {campground: foundCampground});
+      res.render("campgrounds/show", {campground: foundCampground});
     }
   });
+})
+
+
+
+//========================//
+//Comments Routes
+//=========================//
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+  res.render("comments/new")
 })
 
 
