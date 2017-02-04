e743060afc924f6fe2bb8a754747e33ee92fdc1d
Mark Sarpong <markus20@outlook.com>
2017-02-04 15:16:49 +0000

    user authorization and authentication to edit and delete comment

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -96,7 +96,7 @@ router.put("/:id", function(req, res){
 
 
 //DESTROY CAMPGROUND ROUTE
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
   Campground.findByIdAndRemove(req.params.id, function(err){
     if(err){
       res.redirect("/campgrounds");
--- routes/comments.js
+++ routes/comments.js
@@ -50,7 +50,7 @@ router.post("/", isLoggedIn, function(req, res){
 
 //EDIT ROUTE
 
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
     Comment.findById(req.params.comment_id, function(err, foundComment){
       if(err){
         res.redirect("back");
@@ -63,7 +63,7 @@ router.get("/:comment_id/edit", function(req, res){
 
 
 //UPDATE ROUTE
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
 
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
       if(err){
@@ -77,12 +77,12 @@ router.put("/:comment_id", function(req, res){
 
 
 
-router.delete(":/comments_id", function(req, res){
+router.delete("/:comments_id", checkCommentOwnership, function(req, res){
   Comment.findByIdAndRemove(req.params.comments_id, function(err){
     if(err){
       res.redirect("back");
     }else{
-      res.redirect("/campgrounds" + req.params.id);
+      res.redirect("/campgrounds/" + req.params.id);
     }
   })
 
@@ -97,6 +97,26 @@ function isLoggedIn(req, res, next){
   }
   res.redirect("/login");
 
+}
+
+
+function checkCommentOwnership(req, res, next){
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comments_id, function(err, foundComment){
+      if(err){
+        res.redirect("back");
+      }else{
+        //check if the user created the comment
+        if(foundComment.author.id.equals(req.user._id)){
+          next();
+        }else{
+          res.redirect("back");
+        }
+      }
+    })
+  }else{
+    res.redirect("back");
+  }
 }
 
 
